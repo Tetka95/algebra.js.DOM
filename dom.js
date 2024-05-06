6f56cfd59b490568a8dae8306787deefb3f10945
Tetka95 <martina.bobek95@gmail.com>
2024-05-06 18:22:20 +0200

    dovršen zadatak

--- dom.js
+++ dom.js
@@ -7,16 +7,21 @@ console.log('Domena je: ' + window.location.hostname);
 console.log('Window location je: ' + window.location);
 
 var x = window.confirm('Ovime prihvaćate našu politiku o kolačićima.');
-console.log("response = " + x);
+console.log('response = ' + x);
 if (x == true) {
-    console.log("korisnik odgovorio ok");
+    console.log('korisnik odgovorio ok');
 } else {
     console.log('korisnik odgovorio nok');
     window.location = "onama.html";
 }
 
-
 //window.location = 'www.google.com'; --> ne radi zbog sigurnosti
 //window.location = "onama.html"; //--> ovo bi radilo jer se odnosi na stranicu unutar naše stranice
 
-window.confirm
+console.log('end page 1');
+//window.alert('test alert');
+console.log('end page 2');
+
+window.setTimeout(function () {
+    console.log('ispis delay 2 sekunde');
+}, 2000);
