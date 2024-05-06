cc8ba2ad906d980ae72a6a2bb8e21b6dd57e81b6
Tetka95 <martina.bobek95@gmail.com>
2024-05-06 19:21:49 +0200

    zadatak...

---
+++ script.js
@@ -0,0 +1,14 @@
+var h1 = document.createElement("h1");
+h1.appendChild(document.createTextNode('neki tekst u h1'));
+var dDescr = document.querySelector(".description");
+
+dDescr.parentNode.prepend(h1);
+
+var h2 = document.createElement(h2);
+h2.appendChild(document.createTextNode('neki tekst u h2'))
+
+dDescr.parentNode.append(h2);
+
+var li = document.createElement("li");
+var ul = document.querySelector(".ul");
+ 
