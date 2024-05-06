755257e8d7f70e6d1fc4b1dc86df6244bf59a4d4
Tetka95 <martina.bobek95@gmail.com>
2024-05-06 17:43:14 +0200

    window

--- dom.js
+++ dom.js
@@ -1,1 +1,22 @@
+console.log('Unutarnja visina prozora je: ' + window.innerHeight);
+console.log ('Unutarnja širina je: ' + window.innerWidth);
 
+console.log('Domena je: ' + window.location.hostname);
+
+//var winLocation = window.location;
+console.log('Window location je: ' + window.location);
+
+var x = window.confirm('Ovime prihvaćate našu politiku o kolačićima.');
+console.log("response = " + x);
+if (x == true) {
+    console.log("korisnik odgovorio ok");
+} else {
+    console.log('korisnik odgovorio nok');
+    window.location = "onama.html";
+}
+
+
+//window.location = 'www.google.com'; --> ne radi zbog sigurnosti
+//window.location = "onama.html"; //--> ovo bi radilo jer se odnosi na stranicu unutar naše stranice
+
+window.confirm
