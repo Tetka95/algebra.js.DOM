77b873af91e25fe85c117514e9535920b8b5369a
Tetka95 <martina.bobek95@gmail.com>
2024-05-08 18:11:36 +0200

    zadatak

---
+++ index3.js
@@ -0,0 +1,11 @@
+var h1 = document.createElement("h1");
+h1.appendChild(document.createTextNode("Karlovac"));
+
+var bodyElem = document.querySelector("body");
+bodyElem.prepend(h1);
+
+var rijeke = ["Mrežnica", "Kupa", "Korana", "Dobra", "Karlovačko"];
+
+var dDescription = document.querySelector(".description");
+
+dDescription_innerHTML = "Grad na 5 rijeka</h3>"
