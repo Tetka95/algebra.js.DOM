b93de8f476c6cf5dd195bf0d5a9a688eae9ea212
Tetka95 <martina.bobek95@gmail.com>
2024-05-08 16:54:14 +0200

    komentari, moguća rješenja

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ h1.appendChild(document.createTextNode('neki tekst u h1'));
 var dDescr = document.querySelector(".description");
 dDescr.parentNode.prepend(h1);
 
+/*
+var bodyElem = document.querySelector("body");
+console.log(bodyElem);
+bodyElem.prepend(h1);
+---> još jedan način kako riješiti zadatak
+*/ 
+
+/*
 var h2 = document.createElement("h2");
 h2.appendChild(document.createTextNode('neki tekst u h2'))
 
 dDescr.parentNode.append(h2);
+---> umetanje elementa ispod div-a description
+*/
 
 var divInfo = document.createElement("div");
 divInfo.classList.add("info");
