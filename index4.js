5c6168481139215fd3ffbfc2473d36db3ad55e76
Tetka95 <martina.bobek95@gmail.com>
2024-05-08 19:07:45 +0200

    uređivanje liste

---
+++ index4.js
@@ -0,0 +1,9 @@
+var ul = document.querySelector("ul");
+//ul.style.listStyle = "decimal"; --> jedno od rješenja
+
+var i = 1;
+for (let elem of ul.children) {
+    elem.style.listStyle = "decimal";
+    //elem.textContent = i + ". " + elem.textContent; --> ako je listStyle "none" dodamo i ovaj dio koda
+    i++;
+} 
