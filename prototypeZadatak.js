a7a00907e34ba867f1702a7a28da902f9f5ba8fa
Tetka95 <martina.bobek95@gmail.com>
2024-05-14 19:00:49 +0200

    ...

--- prototypeZadatak.js
+++ prototypeZadatak.js
@@ -4,6 +4,10 @@ function Osoba () {
 }
 
 var osoba1 = new Osoba ();
+osoba1.brojCipela = 38;
+console.log("Osoba1 --> ime: " + osoba1.ime + "; godine: " + osoba1.godine
++ "; broj cipela: " + osoba1.brojCipela);
+
 console.log("Osoba1 --> ime: " + osoba1.ime + "; godine: " + osoba1.godine);
 
 Osoba.prototype.spol = "Žensko";
