c3bef1e3eb36c913ef3376b64611b0cb6f6bb186
Tetka95 <martina.bobek95@gmail.com>
2024-05-14 18:55:45 +0200

    vježbaaa

---
+++ prototypeZadatak.js
@@ -0,0 +1,8 @@
+// constructor Osoba
+function Osoba () {
+    (this.ime = "Ana"), (this.godine = 25); 
+}
+
+var osoba1 = new Osoba ();
+console.log("Osoba1 --> ime: " + osoba1.ime + "; godine: " + osoba1.godine);
+
