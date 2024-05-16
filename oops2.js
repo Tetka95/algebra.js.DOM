1c2a4066fbad94dff27946d42424f0a7b5135e92
Tetka95 <martina.bobek95@gmail.com>
2024-05-16 17:31:59 +0200

    pattern funkcija print

--- oops2.js
+++ oops2.js
@@ -10,6 +10,10 @@ Pozovite ju iz novostvorenog objekta i ispišite rezultat.
 */
 
 //1.
+function print(intext){
+    console.log(intext);
+}
+
 function Dog(name) {
     //this.dogName = name || "Rex"; --> skraćeno rješenje za if petlju
     if(!name) {
@@ -22,16 +26,16 @@ this.bojaPsa = "crna"; ///dodano svojstvo koje nije funkcija (sve se može)
 
 //2.
 var dog = new Dog();
-console.log("Ime psa (default) je " + dog.dogName); 
+print("Ime psa (default) je " + dog.dogName); 
 
 var dog1 = new Dog("Floki");
-console.log("Ime psa je " + dog1.dogName);
+print("Ime psa je " + dog1.dogName);
 
 //3.
 Dog.prototype.imePsa = function(){
     return this.dogName;
 }
-console.log("Ime psa(default) je " + dog.imePsa());
+console.log("Ime psa(default) je " + dog.imePsa()); //console.log ili print, radi oboje zbog funkcije dodane na početak
 console.log("Ime psa(određeno) je " + dog1.imePsa());
 
 //4. Dodaj funkciju bark
@@ -39,6 +43,6 @@ Dog.prototype.bark = function(){
     return "woof woof";
 }
 
-console.log("Pas(default) se glasa " + dog.bark());
-console.log("Pas(određeno) se glasa " + dog1.bark());
+print("Pas(default) se glasa " + dog.bark());
+print("Pas(određeno) se glasa " + dog1.bark());
 
