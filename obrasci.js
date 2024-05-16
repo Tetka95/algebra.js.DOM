f0b7f4fde2b90c79160bb5c79aee11cad957c599
Tetka95 <martina.bobek95@gmail.com>
2024-05-16 17:49:40 +0200

    novi zadatak

---
+++ obrasci.js
@@ -0,0 +1,9 @@
+/*
+1. Napišite modul za brojanje koraka.
+Modul će brinuti o povećanju brojanja koraka,
+a izvana će se moći pristupiti dvijema metodama modula:
+walk() i getTotalSteps().
+
+2. Exportajte modul iz datoteke.
+*/
+
