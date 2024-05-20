7cb215abefe26ce0cbfdc71f73fe677d12a59261
Tetka95 <martina.bobek95@gmail.com>
2024-05-20 17:50:34 +0200

    promises i update dog.js

--- dog.js
+++ dog.js
@@ -1,4 +1,5 @@
-class Dog{
+class Dog {
+    constructor(){}
     imePsa = "Rex";
     vrsta = "Seoski pas";
     godine = 1;
@@ -6,7 +7,22 @@ class Dog{
 }
 
 class GoldenRetriever extends Dog {
-    constructor(godine) {
-        this.godine = 4;
+    constructor(godine, vrsta) {
+        super();
+        this.godine = godine;
+        this.vrsta = vrsta;
     }
 }
+
+const dog1 = new Dog();
+const dog2 = new GoldenRetriever(10, "Zlatni retriver");
+
+console.log("Dog1 name = " + dog1.imePsa);
+console.log("Dog1 godine = " + dog1.godine);
+console.log("Dog1 vrsta = " + dog1.vrsta);
+
+console.log("----------");
+
+console.log("Dog2 name = " + dog2.imePsa);
+console.log("Dog2 godine = " + dog2.godine);
+console.log("Dog2 vrsta = " + dog2.vrsta);
